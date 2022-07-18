d87138ab686437d3c01292ee4c981c574ca5f59e
basel mohsen <105518530+baselmohsen@users.noreply.github.com>
2022-07-18 20:19:01 +0200

    Update _aside.blade.php

--- resources/views/layouts/admin/_aside.blade.php
+++ resources/views/layouts/admin/_aside.blade.php
@@ -16,7 +16,9 @@
         
         <li><a class="app-menu__item {{ request()->is('*home*') ? 'active' : '' }}" href="{{route('dashboard.home')}}"><i class="app-menu__icon fa fa-home"></i><span class="app-menu__label">{{ trans('site.home') }}</span></a></li>
        
-      
+        @if (auth()->user()->isAbleTo('read_users'))
+        <li><a class="app-menu__item {{ request()->is('*users*') ? 'active' : '' }}" href="{{route('dashboard.users.index')}}"><i class="app-menu__icon fa fa-user"></i><span class="app-menu__label">{{ trans('site.users') }}</span></a></li>     
+        @endif
 
         @if (auth()->user()->isAbleTo('read_categories'))
         <li><a class="app-menu__item {{ request()->is('*categories*') ? 'active' : '' }}" href="{{route('dashboard.categories.index')}}"> <i class="app-menu__icon fa fa-list" aria-hidden="true"></i>
@@ -28,9 +30,7 @@
              <span class="app-menu__label"> {{   trans('site.products') }}  </span></a></li>     
         @endif
 
-        @if (auth()->user()->isAbleTo('read_users'))
-        <li><a class="app-menu__item {{ request()->is('*users*') ? 'active' : '' }}" href="{{route('dashboard.users.index')}}"><i class="app-menu__icon fa fa-user"></i><span class="app-menu__label">{{ trans('site.users') }}</span></a></li>     
-        @endif
+      
 
         @if (auth()->user()->isAbleTo('read_clients'))
         <li><a class="app-menu__item {{ request()->is('*clients*') ? 'active' : '' }}" href="{{route('dashboard.clients.index')}}"> <i class="app-menu__icon fa fa-users" aria-hidden="true"></i>
