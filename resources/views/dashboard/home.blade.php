79424dd511b6865bdca2f33c42e3f7ad7136c8cb
basel mohsen <105518530+baselmohsen@users.noreply.github.com>
2022-07-18 20:17:37 +0200

    Update home.blade.php

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -1,10 +1,12 @@
 @extends('layouts.admin.app')
 
 @section('content')
+
+
     <div class="container">
 
    
-        {{-- <div class="app-title">
+        <div class="app-title">
           <div>
             <h1><i class="fa fa-dashboard"></i> Dashboard</h1>
             <p>A free and open source Bootstrap 4 admin template</p>
@@ -13,7 +15,7 @@
             <li class="breadcrumb-item"><i class="fa fa-home fa-lg"></i></li>
             <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
           </ul>
-        </div> --}}
+        </div> 
         <div class="row">
           <div class="col-md-6 col-lg-3">
             <div class="widget-small primary coloured-icon"><i class="icon fa fa-list fa-3x"></i>
